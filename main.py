6a9b722becacb4743fdd85d9ab3152e9a03f3347
JimboJon <58301883+JimboJon@users.noreply.github.com>
2019-11-28 15:55:08 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+mood = input("How are you feeling today? ")
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Here's a knife to slice open your wrists")
+elif mood == "excited":
+    print("Must be from all the porn you're watching.")
+elif mood == "relaxed":
+    print("Do you have more of those drugs?")
+else:
+    print("I don't recognize this mood")
+
