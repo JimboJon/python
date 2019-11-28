fe044725d3b4f11ea5d47f89a62f1071a66ec2aa
JimboJon <58301883+JimboJon@users.noreply.github.com>
2019-11-28 17:26:11 +0100

    Add files via upload

---
+++ secretnumber.py
@@ -0,0 +1,11 @@
+secret_number = "5"
+guess = input("Guess my putain secret number")
+if guess == secret_number:
+    print("you are fucking amazing!")
+else:
+    print("Nothing is lost my fair boy. You can guess again")
+
+
+
+
+
