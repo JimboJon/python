09a22d704baa2ade0fe4a72b4f40413de0542e6e
JimboJon <58301883+JimboJon@users.noreply.github.com>
2019-11-28 17:51:40 +0100

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+number1 = int(input("Choose a number "))
+number2 = int(input("choose a second number "))
+operation = input("choose what you wanna do with the numbers ")
+if operation == "+":
+    print(number1+number2)
+if operation == "-":
+    print(number1-number2)
+if operation == "*":
+    print(number1*number2)
+if operation == "/":
+    print(number1/number2)
